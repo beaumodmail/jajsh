7489b35e2f1f2399ee4d32a05723e520c1cf2a1e
beaumodmail <164736839+beaumodmail@users.noreply.github.com>
2024-11-20 18:39:11 +0000

    Update index.js

--- pages/chriso/index.js
+++ pages/chriso/index.js
@@ -6,7 +6,11 @@ export default function ShowImage() {
         <h2>WARNING: All image rights are from the owner.</h2>
         <p>Image Owner: Beau</p>
         <img
-          src="/LOOP-5.jpg" // Path to the image in the public folder
-          alt=""
+          src="/LOOP-5.jpg"  {/* Path to the image in the public folder */}
+          alt="Uploaded Image"
           style={{ maxWidth: '100%', height: 'auto' }}
         />
+      </div>
+    </div>
+  );
+}
