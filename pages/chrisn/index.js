eb8ef214e5d2f17413cffe8fcb09d9e61d0c68d1
beaumodmail <164736839+beaumodmail@users.noreply.github.com>
2024-11-20 18:23:37 +0000

    Update index.js

--- pages/chrisn/index.js
+++ pages/chrisn/index.js
@@ -1,22 +1,22 @@
-// pages/showImage.js
 export default function ShowImage() {
   return (
     <div style={{ fontFamily: 'Arial, sans-serif' }}>
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h1>FILE UPLOAD</h1>
-      <h2>WARNING: All image rights are from the owner.</h2>
-      <p>Image Owner: Beau</p>
-      <img
-        src="/IMG_6226.jpeg"  // Path to the image in the public folder
-        alt="Don the Hairdresser"
-        style={{ maxWidth: '100%', height: 'auto' }}
-      />
-     <h2>AI VERSION OF ORIGINAL; ORIGINAL ABOVE:</h2>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <h1>FILE UPLOAD</h1>
+        <h2>WARNING: All image rights are from the owner.</h2>
+        <p>Image Owner: Beau</p>
         <img
-        src="/IMG_2324.jpeg"  // Path to the image in the public folder
-        alt="Dom the Hairdresser AI"
-        style={{ maxWidth: '100%', height: 'auto' }}
-      />
+          src="/IMG_6226.jpeg" // Path to the image in the public folder
+          alt="Don the Hairdresser"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
+        <h2>AI VERSION OF ORIGINAL; ORIGINAL ABOVE:</h2>
+        <img
+          src="/IMG_2324.jpeg" // Path to the image in the public folder
+          alt="Dom the Hairdresser AI"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
+      </div>
     </div>
   );
 }
