1b31af2a36f88716b18d7a9708e26c71a9873469
beaumodmail <164736839+beaumodmail@users.noreply.github.com>
2024-11-20 18:01:50 +0000

    Update index.js

--- pages/chrisn/index.js
+++ pages/chrisn/index.js
@@ -3,10 +3,12 @@
 export default function ShowImage() {
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h1>Here is the image:</h1>
+      <h1>FILE UPLOAD</h1>
+      <h2>WARNING: All image rights are from the owner.</h2>
+      <p>Image Owner: Beau
       <img
         src="/IMG_6226.jpeg"  // Path to the image in the public folder
-        alt="Uploaded Image"
+        alt="Don the Hairdresser"
         style={{ maxWidth: '100%', height: 'auto' }}
       />
     </div>
