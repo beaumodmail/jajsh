cd471cc22061d232aced4610ab5e9c0b9018d1bb
beaumodmail <164736839+beaumodmail@users.noreply.github.com>
2024-12-06 21:48:03 +0000

    Update index.js

--- pages/ex/1/index.js
+++ pages/ex/1/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000; // You can choose any port
+// next.js/pages/index.js
+import { useEffect } from 'react';
 
-app.get('/', (req, res) => {
-    res.redirect('http://thebeauclub.byethost15.com/videos/exclusive');
-});
+export default function Home() {
+  useEffect(() => {
+    window.location.href = 'http://thebeauclub.byethost15.com/videos/exclusive';
+  }, []);
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+  return null;
+}
+
 
